d9fb3b6bc5857e7d8cec413160dc9d19af6ec2bf
Evan <evanacox00@gmail.com>
2019-05-11 21:37:51 -0400

    Init v2

---
+++ precache-manifest.a102fd869f63312e56cdfa19402ff0ba.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26d980f536e350ce047dc43d320be05d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd17495da8a645ec904b",
+    "url": "/static/js/2.b33bd1e5.chunk.js"
+  },
+  {
+    "revision": "245d59c774f9cbda839a",
+    "url": "/static/js/main.402a675e.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  }
+]);
